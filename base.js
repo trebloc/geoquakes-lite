b44092074608fe29a4708a693b0a49773f042d92
trebloc <ctse77@alum.berkeley.edu>
2016-03-21 17:23:07 -0700

    added google maps marker

--- base.js
+++ base.js
@@ -14,17 +14,19 @@ $.ajax({
     url: weekly_quakes_endpoint,
     success: function (data) {
       data.features.forEach(function (element){
-        var results = element.properties.title;
-        var longitutde = element.geometry.coordinates[0];
-        var latitude = element.geometry.coordinates[1];        
+        var title = element.properties.title;
+        var lng = element.geometry.coordinates[0];
+        var lat = element.geometry.coordinates[1];
+        new google.maps.Marker({
+          position: new google.maps.LatLng(lat,lng),
+          map: map,
+          title: title
+        });        
         var mag = element.properties.mag;
         var time = Math.round( ( Date.now() - element.properties.time ) / (1000*60*60) );
-			// $('#info').append("<p>" + location + "</p>");
-			// $('#info').append("<p>" + "latitude: " + latitude + "</p>");
-			// $('#info').append("<p>" + "longitutde: " + longitutde + "</p>");	
-			// $('#info').append("<p>" + "time of event: " + time + " hours ago" +"</p>");		
+		
     
-      $('#info').append("<p>" + results + " Longitutde - " + longitutde + " Latitude - " + latitude + "<br>" + " Time - " + time + " hours ago</p>");
+      $('#info').append("<p>" + title + " Longitude: " + lng + " Latitude: " + lat + "<br>" + " Time: " + time + " hours ago</p>");
       });
     }
   });
@@ -35,11 +37,7 @@ $.ajax({
 
 // This is a code snippet that will place a pin at the coordinates lat & lng
 
-new google.maps.Marker({
-  position: new google.maps.LatLng(lat,lng),
-  map: map,
-  title: title
-});
+
 
 
 /*
@@ -69,3 +67,8 @@ $.ajax({
         // time = time - Date.now();
         // time = time / 1000 / 60 / -60;
         // time = Math.round(time);
+
+              // $('#info').append("<p>" + location + "</p>");
+      // $('#info').append("<p>" + "latitude: " + latitude + "</p>");
+      // $('#info').append("<p>" + "longitutde: " + longitutde + "</p>"); 
+      // $('#info').append("<p>" + "time of event: " + time + " hours ago" +"</p>");
